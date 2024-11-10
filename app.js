73844c795424acb13c780fee074afd4af7c6a818
dwijverma <dwijverma2002@gmail.com>
2024-11-10 12:05:09 +0530

    updated cors for testing

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ require("dotenv").config();
 const connectDB = require("./db/connect.db.js");
 const routes = require("./routes/routes.js");
 const authRoutes = require("./routes/authRoutes.js");
-const projectPlanningRoutes = require("./routes/projectPlanningRoutes.js")
-const critiqueRoutes = require("./routes/critiqueRoutes.js")
-const colorPaletteRoutes = require("./routes/color-paletteRoutes.js")
+const projectPlanningRoutes = require("./routes/projectPlanningRoutes.js");
+const critiqueRoutes = require("./routes/critiqueRoutes.js");
+const colorPaletteRoutes = require("./routes/color-paletteRoutes.js");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,19 +25,23 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
+// tempory allow access from anywhere -
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/", routes);
 app.use("/auth", authRoutes);
 app.use("/critique", critiqueRoutes);
 app.use("/planning", projectPlanningRoutes);
-app.use("/color-palette", colorPaletteRoutes)
+app.use("/color-palette", colorPaletteRoutes);
 
 connectDB()
   .then(() => {
