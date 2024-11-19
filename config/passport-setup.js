c784f14f94b465536bb64ee007edf89b9ad44c28
dwijverma <dwijverma2002@gmail.com>
2024-11-19 16:27:34 +0530

    adding callback url to .env

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,17 +1,18 @@
 // config/passport-setup.js
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config()
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const CALLBACK_URL = process.env.CALLBACK_URL;
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: `${GOOGLE_CLIENT_ID}`,
       clientSecret: `${GOOGLE_CLIENT_SECRET}`,
-      callbackURL: '/auth/google/callback',
+      callbackURL: CALLBACK_URL,
     },
     (token, tokenSecret, profile, done) => {
       return done(null, profile);
