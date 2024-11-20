4bad94309471d7c4faef1a8c14d80ac48fc9e740
dwijverma <dwijverma2002@gmail.com>
2024-11-20 16:49:37 +0530

    shifting cors config to top level

--- app.js
+++ app.js
@@ -15,26 +15,6 @@ const colorPaletteRoutes = require("./routes/color-paletteRoutes.js");
 const app = express();
 const port = process.env.PORT || 2000;
 
-app.use(
-  cookieSession({
-    name: "session",
-    keys: ["key1", "key2"],
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
-
-// tempory allow access from anywhere -
-// app.use(cors());
-
 const corsOptions = {
   origin: "https://www.designershangout.com", // Replace with your frontend domain
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -47,6 +27,16 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/hello", async (req, res) => {
   res.status(200).json({ msg: "hello world" });
