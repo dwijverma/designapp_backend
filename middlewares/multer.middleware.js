cb9b619d1616c32ffd81b554f95e95ac6af7bfdf
dwijverma <dwijverma2002@gmail.com>
2024-12-01 04:59:55 +0530

    ui changes and backend changes

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,4 +1,14 @@
 const multer = require("multer");
+
+const fs = require("fs");
+const path = require("path");
+
+const tempDir = path.join(__dirname, "./public/temp");
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
