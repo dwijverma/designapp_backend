573b4ea611f6adc1c1c7e8be5e537b7941e4fa23
dwijverma <dwijverma2002@gmail.com>
2024-12-01 04:15:42 +0530

    ui changes and backend changes

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cookieSession({
     name: "session",
